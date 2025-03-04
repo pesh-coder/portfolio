eb27d59d2145a1baa0b71916d1862d523edc2fc8
Sitati Patience <patiencesitati99@gmail.com>
2025-03-04 15:19:19 +0300

    Update marks_assign.py

--- marks_assign.py
+++ marks_assign.py
@@ -18,8 +18,18 @@ print(type(word))
 print (word [0])
 
 #set
+#set data type - when you don't want duplicates and want random access to the data
+student_id = {11, 2, 31, 44, 5, 6, 7, 88, 9, 102}
 
 #boolean
+#True or False
+comp1, comp2 = 100, 200
+#less than to
+print(comp1 < comp2) 
+#greater than to
+print(comp1 > comp2) 
+ #equal to
+print(comp1 == comp2)
 
 #sequence
 #list
@@ -31,6 +41,13 @@ num1 = [100, 300, 500]
 numbers2 = (num1, num2, num3)
 
 #range
+def enter_number():
+    number = int(input("the number of entries"))
+    capture = {input("enter the key: "): input("enter value.") for _ in range(number)}
+    
+    print(capture)
+
+enter_number()    
 
 #dictionary
 student_detail = {"name": "patience", "track": "python", "gender": "female", "age": 25}
@@ -48,6 +65,9 @@ print(student_detail.values())
 #giving the user the ability to input a number
 num1  = input ("input your number here")
 print ("your number is: ", num1)
+
+# \n takes the output to be printed in the next line
+a = int(input("please enter your first number:\n "))
 
 
 
